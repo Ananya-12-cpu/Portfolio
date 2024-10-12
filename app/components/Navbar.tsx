438f1e8cf2e8326b67518bef331a4b9a0ab6d0be
Ananya Putatunda <ananya200696@gmail.com>
2024-10-12 16:14:41 +0530

    link on logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -16,7 +16,10 @@ const Navbar = () => {
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <div className="flex-shrink-0">
-              <h1 className="text-white text-xl font-bold">MyLogo</h1>
+              {/* <h1 className="text-white text-xl font-bold">MyLogo</h1> */}
+              <h1 className="text-white text-xl font-bold">
+                <Link href={"/"}>MyLogo</Link>
+              </h1>
             </div>
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
