0303cfd58dbdc06588fc9eeac085ad318acff63e
Ananya Putatunda <ananya200696@gmail.com>
2024-10-12 16:10:22 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+
+import About from "./about/page";
+import PortfolioBanner from "./components/Banner";
+import Contact from "./contact/page";
+
+export default function Home() {
+  return (
+    <div>
+      
+    <PortfolioBanner/>
+    <About/>
+    <Contact/>
+    </div>
+  );
+}
